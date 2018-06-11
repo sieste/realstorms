a3bbfd944da27a3b66ed623fe5364f23877fb987
Stefan Siegert <stefan_siegert@gmx.de>
2018-06-11 12:18:07 +0100

    smooth contour lines

--- R/plot_density.R
+++ R/plot_density.R
@@ -2,17 +2,23 @@
 #'
 #' @param obj Data table returned by function `count_storms`
 #' @param what One of 'track_density', 'feature_density', 'track_count', 'feature_count'
+#' @param contour_smooth Smoothing parameter for the contour lines
+#' @param contour_levels Vector of levels at which to draw contour lines.
 #' @return A ggplot2 object
 #' @seealso track_density
 #' @import ggplot2
 #' @import viridis
 #' @importFrom broom tidy
+#' @importFrom smoothie gauss2dsmooth
 #' @export
 #'
 plot.track_density =
 function(
   obj, 
-  what = c('track_density', 'feature_density', 'track_count', 'feature_count')) 
+  what = c('track_density', 'feature_density', 'track_count', 'feature_count'),
+  contour_smooth = NULL,
+  contour_levels = NULL
+) 
 {
 
   what = match.arg(what)
@@ -20,9 +26,18 @@ function(
   data('coastsCoarse', package='rworldmap')
   coastline = tidy(coastsCoarse)
 
+  # data table to matrix, then apply smoothie::gauss2dsmooth
+  if (!is.null(contour_smooth)) {
+    mat = as.matrix(dcast(obj, lon ~ lat, value.var=what), rownames=TRUE)
+    smoo = as.vector(smoothie::gauss2dsmooth(mat, lambda=contour_smooth, 
+                                             nx=ncol(mat), ny=nrow(mat)))
+  } else {
+    smoo = obj[, get(what)]
+  }
+  
   plt = ggplot(obj, aes(x=lon, y=lat)) + 
     geom_raster(aes_string(fill=what, alpha=what), interpolate=TRUE) +
-    geom_contour(aes_string(z=what, colour='..level..')) +
+    geom_contour(aes_string(z=smoo, colour='..level..'), breaks=contour_levels) +
     geom_path(data=coastline, aes(x=long, y=lat, group=group), color='black') +
     scale_alpha_continuous(range=c(0, 1), guide='none') +
     scale_fill_viridis() +
